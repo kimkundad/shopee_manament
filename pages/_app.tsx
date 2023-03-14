a6b5aefb52644be68b684a32260a2cc1c4809f92
infree002 <piyachat.sng@gmail.com>
2023-03-14 12:55:02 +0700

    update stock management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "components/layout/Layout";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#44337A",
+      100: "#B794F4",
+      500: "#3FFF33", // you need this
+    }
+  }
+});
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/stock.js
+++ pages/stock.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { Box, Switch, Text } from "@chakra-ui/react";
-
+import {
+  Link,
+  Flex,
+  Switch,
+  Text,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Box,
+  Spacer,
+  Checkbox,
+  Menu,
+  MenuButton,
+  MenuList,
+  Button,
+  MenuItem,
+  CheckboxGroup,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 import { Table } from "@nextui-org/react"; // icons
 export default function stock() {
   const products = [
     {
       id: "sadasld",
-      image: "dfjhafja",
+      image: "/images/edit.png",
       name: "dfjshsd",
       quantity: 100,
       cost: 120,
@@ -16,7 +34,7 @@ export default function stock() {
     },
     {
       id: "sadasld",
-      image: "dfjhafja",
+      image: "/images/edit.png",
       name: "dfjshsd",
       quantity: 100,
       cost: 120,
@@ -26,7 +44,7 @@ export default function stock() {
     },
     {
       id: "sadasld",
-      image: "dfjhafja",
+      image: "/images/edit.png",
       name: "dfjshsd",
       quantity: 100,
       cost: 120,
@@ -90,61 +108,209 @@ export default function stock() {
       setIsCheckedAll(false);
     }
   };
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
+  const handleOptionChange = (option) => {
+    if (selectedOptions.includes(option)) {
+      setSelectedOptions(selectedOptions.filter((item) => item !== option));
+    } else {
+      setSelectedOptions([...selectedOptions, option]);
+    }
+  };
   return (
     <>
-      <Table
-        striped
-        sticked
-        aria-label="Example table with static content"
-        css={{
-          height: "auto",
-          minWidth: "100%",
-          border: "0px",
-        }}
-      >
-        <Table.Header bg="red">
-          {colunm.map((item, index) => {
-            return (
-              <Table.Column
-                style={{ backgroundColor: "red", color: "white" }}
-                key={index}
-                onClick={(event) => handleAllSwitchChange()}
-              >
-                <Text>{item.label}</Text>
+      <Box>
+        <Flex m="10px" pt="10px">
+          <Box>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Image src="/images/search.png" h="20px" w="20px" />
+              </InputLeftElement>
+              <Input
+                borderRadius="3xl"
+                type="text"
+                fontSize="21px"
+                borderColor="gray.500"
+                placeholder="ค้นหารายการ"
+              />
+            </InputGroup>
+          </Box>
+          <Spacer />
+          <Box borderWidth="1px" borderColor="red" borderRadius="md">
+            <Button
+              fontSize="21px"
+              leftIcon={<Image src="/images/pluswhite.png" h="25px" w="25px" />}
+              bg="red"
+              variant="solid"
+              color="white"
+            >
+              เพิ่มสินค้า
+            </Button>
+          </Box>
 
-                {index == 0 ? (
-                  <Switch colorScheme="green" isChecked={isCheckedAll} />
-                ) : null}
-              </Table.Column>
-            );
-          })}
-        </Table.Header>
-        <Table.Body>
-          {products.map((item, index) => {
-            return (
-              <Table.Row key={index}>
-                <Table.Cell>
-                  <Switch
-                    colorScheme="green"
-                    isChecked={isCheckedOne[index]}
-                    onChange={(event) => handleOneSwitchChange(index)}
+          <Box ml="5px" border="1px" borderColor="red" borderRadius="md">
+            <Menu closeOnSelect={false}>
+              <MenuButton
+                as={Button}
+                bg="white"
+                fontSize="21px"
+                leftIcon={<Image src="/images/menu.png" h="25px" w="25px" />}
+                rightIcon={
+                  <Image
+                    src="/images/down-filled-triangular-arrow.png"
+                    h="10px"
+                    w="20px"
                   />
-                </Table.Cell>
-                <Table.Cell>{item.id}</Table.Cell>
-                <Table.Cell>{item.image}</Table.Cell>
-                <Table.Cell>{item.name}</Table.Cell>
-                <Table.Cell>{item.quantity}</Table.Cell>
-                <Table.Cell>{item.cost}</Table.Cell>
-                <Table.Cell>{item.price}</Table.Cell>
-                <Table.Cell>{item.created_date}</Table.Cell>
-                <Table.Cell>{item.maker}</Table.Cell>
-                <Table.Cell>a</Table.Cell>
-              </Table.Row>
-            );
-          })}
-        </Table.Body>
-      </Table>
+                }
+              >
+                เลือกตัวแสดงผล
+              </MenuButton>
+              <MenuList
+                minWidth="200px"
+                border="1px"
+                borderColor="red"
+                borderRadius="md"
+              >
+                <CheckboxGroup>
+                  <MenuItem>
+                    <Checkbox
+                      value="item1"
+                      sx={{
+                        ".chakra-checkbox__control": {
+                          background: "white !important",
+                          borderColor: "black !important",
+                          color: "#3FFF33 !important",
+                          border: "1px solid",
+                        },
+                      }}
+                    >
+                      Item 1
+                    </Checkbox>
+                  </MenuItem>
+                  <MenuItem>
+                    <Checkbox
+                      value="item2"
+                      sx={{
+                        ".chakra-checkbox__control": {
+                          background: "white !important",
+                          borderColor: "black !important",
+                          color: "#3FFF33 !important",
+                          border: "1px solid",
+                        },
+                      }}
+                    >
+                      Item 2
+                    </Checkbox>
+                  </MenuItem>
+                  <MenuItem>
+                    <Checkbox
+                      value="item3"
+                      sx={{
+                        ".chakra-checkbox__control": {
+                          background: "white !important",
+                          borderColor: "black !important",
+                          color: "#3FFF33 !important",
+                          border: "1px solid",
+                        },
+                      }}
+                    >
+                      Item 3
+                    </Checkbox>
+                  </MenuItem>
+                </CheckboxGroup>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Flex>
+        <Box m="10px">
+          <Table
+            striped
+            sticked
+            aria-label="Example table with static content"
+            css={{
+              height: "auto",
+              minWidth: "100%",
+              border: "0px",
+              fontSize: "21px",
+              borderCollapse: "collapse",
+              padding: "10px",
+            }}
+          >
+            <Table.Header bg="red">
+              {colunm.map((item, index) => {
+                return (
+                  <Table.Column
+                    style={{
+                      backgroundColor: "red",
+                      color: "white",
+                      fontSize: "21px",
+                    }}
+                    key={index}
+                    onClick={
+                      index == 0 ? (event) => handleAllSwitchChange() : null
+                    }
+                  >
+                    <Text>{item.label}</Text>
+
+                    {index == 0 ? (
+                      <Switch colorScheme="brand" isChecked={isCheckedAll} />
+                    ) : null}
+                  </Table.Column>
+                );
+              })}
+            </Table.Header>
+            <Table.Body>
+              {products.map((item, index) => {
+                return (
+                  <Table.Row
+                    key={index}
+                    css={
+                      index % 2 !== 0
+                        ? {
+                            background: "$gray100 !important",
+                            position: "none !important",
+                          }
+                        : { position: "none !important" }
+                    }
+                  >
+                    <Table.Cell>
+                      <Switch
+                        colorScheme="brand"
+                        isChecked={isCheckedOne[index]}
+                        onChange={(event) => handleOneSwitchChange(index)}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>{item.id}</Table.Cell>
+                    <Table.Cell>
+                      <Image src={item.image} h="30px" w="30px" />
+                    </Table.Cell>
+                    <Table.Cell>{item.name}</Table.Cell>
+                    <Table.Cell>{item.quantity}</Table.Cell>
+                    <Table.Cell>{item.cost}</Table.Cell>
+                    <Table.Cell>{item.price}</Table.Cell>
+                    <Table.Cell>{item.created_date}</Table.Cell>
+                    <Table.Cell>{item.maker}</Table.Cell>
+                    <Table.Cell>
+                      <Flex>
+                        <Link href="/">
+                          <Image src="/images/edit.png" h="25px" />
+                        </Link>
+                        <Link href="/">
+                          <Image
+                            pl="7px"
+                            src="/images/trash-bin.png"
+                            h="25px"
+                          />
+                        </Link>
+                      </Flex>
+                    </Table.Cell>
+                  </Table.Row>
+                );
+              })}
+            </Table.Body>
+          </Table>
+        </Box>
+      </Box>
     </>
   );
 }
