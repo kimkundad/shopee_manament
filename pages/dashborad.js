a6a3eb4ead441b66d21eb11654db0762823501bb
infree002 <piyachat.sng@gmail.com>
2023-05-26 16:17:46 +0700

    update dashborad pie out label

--- pages/dashborad.js
+++ pages/dashborad.js
@@ -34,7 +34,6 @@ export default function DashBoard() {
   const [totalSales, setTotalSales] = useState();
   const [totalDelivery, setTotalDelivery] = useState(null);
   const [totalPayment, setTotalPayment] = useState(null);
-  const chartRefProduct = useRef(null);
   const [startDatePie, setStartDatePie] = useState(
     moment().startOf("month").unix()
   );
@@ -283,42 +282,6 @@ export default function DashBoard() {
     }
   }, [allStock, totalDelivery]);
 
-  const dataPieProduct = {
-    labels: [],
-    datasets: [
-      {
-        label: "My First Dataset",
-        data: dataPieProducts.map((element) => ({
-          value: element.total_num,
-          formattedValue: element.total_num.toLocaleString(),
-        })),
-        backgroundColor: color,
-        borderColor: color,
-        borderWidth: 1,
-      },
-    ],
-    options: {
-      plugins: {
-        legend: false,
-        outlabels: {
-          text: "%l %p",
-          color: "white",
-          stretch: 35,
-          font: {
-            resizable: true,
-            minSize: 12,
-            maxSize: 18,
-          },
-        },
-      },
-    },
-  };
-
-  // Add arrows to the pie chart output values
-  dataPieProduct.datasets[0].data.forEach((dataPoint) => {
-    const arrow = dataPoint.value >= 0 ? "↑" : "↓";
-    dataPoint.formattedValue = arrow + " " + dataPoint.formattedValue;
-  });
   const dataPieShop = {
     labels: [],
     datasets: [
@@ -423,6 +386,8 @@ export default function DashBoard() {
     }
   }, [dataBar]);
 
+  const chartRefProduct = useRef(null);
+  const chartRefShops = useRef(null);
   useEffect(() => {
     const data = {
       labels: dataPieProducts.map((element) => element.name_product),
@@ -504,14 +469,100 @@ export default function DashBoard() {
       },
       plugins: [pieLabelLine],
     };
-
     const chart = new Chart(chartRefProduct.current, config);
+    // Clean up the chart when the component unmounts
+    return () => {
+      chart.destroy();
+      
+    };
+  }, [dataPieProducts,dataPieShops]);
 
+  useEffect(() => {
+    const data = {
+      labels: dataPieShops?.map((element) => element.name_shop),
+      datasets: [
+        {
+          label: "My First Dataset",
+          data: dataPieShops?.map((element) => parseInt(element.total_num)),
+          backgroundColor: color,
+          borderColor: color,
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    const pieShopLabelLine = {
+      id: "pieShopLabelLine",
+      afterDraw(chart, args, options) {
+        const {
+          ctx,
+          chartArea: { top, bottom, left, right, width, height },
+        } = chart;
+
+        chart.data.datasets.forEach((dataset, i) => {
+          chart.getDatasetMeta(i).data.forEach((datapoint, index) => {
+            const { x, y, startAngle, endAngle } = datapoint.getProps([
+              "x",
+              "y",
+              "startAngle",
+              "endAngle",
+            ]);
+
+            const radius = datapoint.outerRadius;
+            const midAngle = (startAngle + endAngle) / 2;
+            const x1 = Math.cos(midAngle) * radius + datapoint.x;
+            const y1 = Math.sin(midAngle) * radius + datapoint.y;
+
+            const xLine = x1 >= width / 2 ? x1 + 15 : x1 - 15;
+            const yLine = y1 >= height / 2 ? y1 + 15 : y1 - 15;
+            const extraLine = x1 >= width / 2 ? 15 : -15;
+
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(xLine, yLine);
+            ctx.lineTo(xLine + extraLine, yLine);
+            ctx.strokeStyle = dataset.borderColor[index];
+            ctx.stroke();
+
+            const textWidth = ctx.measureText(chart.data.labels[index]).width;
+            ctx.font = "15px Arial";
+
+            const textXPosition = x1 >= width / 2 ? "left" : "right";
+            const plushFivePx = x1 >= width / 2 ? 5 : -5;
+            ctx.textAlign = textXPosition;
+            ctx.textBaseline = "middle";
+            ctx.fillStyle = dataset.borderColor[index];
+            ctx.fillText(
+              chart.data.labels[index],
+              xLine + extraLine + plushFivePx,
+              yLine
+            );
+          });
+        });
+      },
+    };
+    const configShops = {
+      type: "pie",
+      data,
+      options: {
+        layout:{
+          padding: 20
+        },
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+      plugins: [pieShopLabelLine],
+    };
+    const chart = new Chart(chartRefShops.current, configShops);
     // Clean up the chart when the component unmounts
     return () => {
       chart.destroy();
     };
-  }, [dataPieProducts]);
+  }, [dataPieShops]);
 
   // Clean up the chart when the component unmounts
   return (
@@ -591,7 +642,8 @@ export default function DashBoard() {
             </Text>
           </Box>
           <Box textAlign="-webkit-center" w="100%" h="300px">
-            <Pie data={dataPieShop} options={optionsPie} />
+            {/* <Pie data={dataPieShop} options={optionsPie} /> */}
+            <canvas ref={chartRefShops}></canvas>
           </Box>
           <Box p="25px" pb="0px">
             <Grid templateColumns="repeat(2, 1fr)" gap={0}>
--- pages/test.js
+++ pages/test.js
@@ -102,7 +102,107 @@ const ChartPage = () => {
     };
   }, []);
 
+  const chartRefa = useRef(null);
+
+  useEffect(() => {
+    const dataa = {
+      labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Suna"],
+      datasets: [
+        {
+          label: "Weekly Sales",
+          data: [18, 12, 6, 9, 12, 3, 9],
+          backgroundColor: [
+            "rgba(255, 26, 104, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(0, 0, 0, 1)",
+          ],
+          borderColor: [
+            "rgba(255, 26, 104, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(0, 0, 0, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    const pieLabelLinea = {
+      id: "pieLabelLinea",
+      afterDraw(chart, args, options) {
+        const {
+          ctx,
+          chartArea: { top, bottom, left, right, width, height },
+        } = chart;
+    
+        chart.data.datasets.forEach((dataset, i) => {
+          chart.getDatasetMeta(i).data.forEach((datapoint, index) => {
+            const { x, y, startAngle, endAngle } = datapoint.getProps(["x", "y", "startAngle", "endAngle"]);
+    
+            const radius = datapoint.outerRadius;
+            const midAngle = (startAngle + endAngle) / 2;
+            const x1 = Math.cos(midAngle) * radius + datapoint.x;
+            const y1 = Math.sin(midAngle) * radius + datapoint.y;
+    
+            const xLine = x1 >= width / 2 ? x1 + 15 : x1 - 15;
+            const yLine = y1 >= height / 2 ? y1 + 15 : y1 - 15;
+            const extraLine = x1 >= width / 2 ? 15 : -15;
+    
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(xLine, yLine);
+            ctx.lineTo(xLine + extraLine, yLine);
+            ctx.strokeStyle = dataset.borderColor[index];
+            ctx.stroke();
+    
+            const textWidth = ctx.measureText(chart.data.labels[index]).width;
+            ctx.font = "12px Arial";
+    
+            const textXPosition = x1 >= width / 2 ? "left" : "right";
+            const plushFivePx = x1 >= width / 2 ? 5 : -5;
+            ctx.textAlign = textXPosition;
+            ctx.textBaseline = "middle";
+            ctx.fillStyle = dataset.borderColor[index];
+            ctx.fillText(
+              chart.data.labels[index],
+              xLine + extraLine + plushFivePx,
+              yLine
+            );
+          });
+        });
+      },
+    };
+    
+    const configa = {
+      type: "pie",
+      dataa,
+      options: {
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+      plugins: [pieLabelLinea],
+    };
+
+    const chart = new Chart(chartRefa.current, configa);
+
+    // Clean up the chart when the component unmounts
+    return () => {
+      chart.destroy();
+    };
+  }, []);
   return (
+    <>
     <div>
       <div className="chartCard">
         <div className="chartBox">
@@ -110,6 +210,14 @@ const ChartPage = () => {
         </div>
       </div>
     </div>
+    <div>
+    <div className="chartCard">
+      <div className="chartBox">
+        <canvas ref={chartRefa}></canvas>
+      </div>
+    </div>
+  </div>
+  </>
   );
 };
 
